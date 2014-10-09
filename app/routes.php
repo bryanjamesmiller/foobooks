ae294b855768ca657eb18cd5d75911d5c9023fea
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-10-09 14:38:43 -0500

    second commit lol

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,29 @@
 |
 */
 
+//We changed the default http path localhost to be equal to localhost/foobooks
+//because we changed the xampp config settings, which
+//I believe is also the httpd.conf file, under the documentRoots settings
+//Now just go to http://localhost and you will arrive at the below page.
 Route::get('/', function()
 {
 	return View::make('hello');
+});
+
+//You have to use the http path:  localhost/books
+Route::get('/books', function() {
+    return 'Here are all the books...';
+});
+
+//You have to use the http path:  localhost/books/whatever
+Route::get('/books/{category}', function($category) {
+    return 'Here are all the books in the category of '.$category;
+});
+
+//You have to use the http path:  localhost/practice
+//They might ask you to "Run echo App::environment(); in your practice route to see
+//how you can get Laravel to tell you what environment you're on" and that just means
+// navigate to http://localhost/practice
+Route::get('/practice', function() {
+    echo App::environment();
 });
