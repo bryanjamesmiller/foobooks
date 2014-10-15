4549ac31dd9202699c5283098f3ad36e8edff543
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-10-14 20:01:56 -0500

    ok

--- app/routes.php
+++ app/routes.php
@@ -1,39 +1,58 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
 
-//We changed the default http path localhost to be equal to localhost/foobooks
-//because we changed the xampp config settings, which
-//I believe is also the httpd.conf file, under the documentRoots settings
-//Now just go to http://localhost and you will arrive at the below page.
-Route::get('/', function()
-{
-	return View::make('hello');
+
+# Homepage
+Route::get('/', function() {
+
+    return View::make('index');
+
 });
 
-//You have to use the http path:  localhost/books
-Route::get('/books', function() {
-    return 'Here are all the books...';
+// List all books / search
+Route::get('/list/{query?}', function($query) {
+
+    return View::make('list');
+
 });
 
-//You have to use the http path:  localhost/books/whatever
-Route::get('/books/{category}', function($category) {
-    return 'Here are all the books in the category of '.$category;
+// Display the form for a new book
+Route::get('/add', function() {
+
+
 });
 
-//You have to use the http path:  localhost/practice
-//They might ask you to "Run echo App::environment(); in your practice route to see
-//how you can get Laravel to tell you what environment you're on" and that just means
-// navigate to http://localhost/practice
-Route::get('/practice', function() {
-    echo App::environment();
+// Process form for a new book
+Route::post('/add', function() {
+
+
+});
+
+// Display the form to edit a book
+Route::get('/edit/{title}', function() {
+
+
+
+});
+
+// Process form for a edit book
+Route::post('/edit/', function() {
+
+
+});
+
+
+
+Route::get('/data', function() {
+
+    // Get the file
+    $books = File::get(app_path().'/database/books.json');
+
+    // Convert to an array
+    $books = json_decode($books,true);
+
+    // Return the file
+    echo Pre::render($books);
+
+
 });
