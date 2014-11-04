14318769d9890c7eaa5f71430cec5f4b7a190886
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-11-03 18:46:06 -0600

    aaa

--- app/views/list.blade.php
+++ app/views/list.blade.php
@@ -6,8 +6,26 @@ List all the books
 
 @section('content')
 <h1>List all books</h1>
-View as:
-	<a href='/list/json' target='_blank'>JSON</a> |
-	<a href='/list/pdf' target='_blank'>PDF</a>
+<div>
+		View as:
+		<a href='/list/json' target='_blank'>JSON</a> |
+		<a href='/list/pdf' target='_blank'>PDF</a>
+	</div>
+
+	@foreach($books as $title => $book)
+		<section class='book'>
+			<h2>{{ $title }}</h2>
+			{{ $book['author'] }} ({{$book['published']}})
+
+			<div class='tags'>
+				@foreach($book['tags'] as $tag)
+					{{ $tag }}
+				@endforeach
+			</div>
+			<img src='{{ $book['cover'] }}'>
+			<br>
+			<a href='{{ $book['purchase_link'] }}'>Purchase...</a>
+		</section>
+	@endforeach
 
 @stop
